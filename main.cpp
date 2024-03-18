44ccebff0b5da6858fbe2474985130565f44f0eb
AdamusPL <adam.czekalski.szczecin@gmail.com>
2024-03-18 21:36:51 +0100

    .txt files

--- main.cpp
+++ main.cpp
@@ -7,11 +7,8 @@
 using namespace std;
 
 int main() {
-    Day3 day3;
-//    day3.part1();
-    day3.part2();
     Day8 day8;
 //    day8.part1();
-//    day8.part2();
+    day8.part2();
     return 0;
 }
